de68a41c8fee651f55894c36c361e1f77d4e65d6
ademirpaj <ademir.paj@hotmail.com>
2018-10-10 11:51:39 -0300

    lala

--- app/src/main/java/br/com/digitalhouse/digitalapp/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/digitalapp/MainActivity.java
@@ -1,11 +1,19 @@
 package br.com.digitalhouse.digitalapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import br.com.digitalhouse.digitalapp.fragments.EventosFragment;
+import br.com.digitalhouse.digitalapp.fragments.PeopleFragment;
+import br.com.digitalhouse.digitalapp.fragments.PostsFragments;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         texto.setText(emailDigitado);
 
+    }
+
+    public void exibiPosts(View view) {
+        FragmentManager manager = getSupportFragmentManager();
+         FragmentTransaction transaction = manager.beginTransaction();
+         transaction.replace(R.id.container_id, new PostsFragments());
+         transaction.commit();
+    }
+
+    public void exibirPessoas(View view) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container_id, new PeopleFragment());
+        transaction.commit();
+
+
+    }
+
+
+
+    public void exibirEventos(View view) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container_id, new EventosFragment());
+        transaction.commit();
     }
 }
